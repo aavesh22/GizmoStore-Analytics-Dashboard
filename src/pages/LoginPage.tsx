41e3ea17bc7f5e277332bb37bc65af68889cfa47
Aavesh Momin <70098026+aavesh22@users.noreply.github.com>
2025-06-15 18:59:27 +0530

    Updated LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -107,7 +107,6 @@ const LoginPage: React.FC = () => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-sm text-gray-900 bg-white"
-                  placeholder="admin@gizmostore.com"
                 />
               </div>
             </div>
@@ -126,7 +125,7 @@ const LoginPage: React.FC = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-sm text-gray-900 bg-white"
-                  placeholder="admin123"
+             
                 />
                 <button
                   type="button"
